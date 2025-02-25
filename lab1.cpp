8482cca37f8a30f1c73d560b73ddacad6dd8ad5e
zeifo8 <leonid.nk2005@gmail.com>
2025-02-25 16:18:57 +0300

    Добавлена запись корней в файл

--- lab1.cpp
+++ lab1.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <cmath>
-
+#include <fstream>
+#include <string>
 using namespace std;
+
+void write_to_file(const string& name, const string& res) {
+	ofstream file(name);
+	file << res;
+	file.close();
+}
 
 void coefficients(double& a, double& b, double& c) {
 	cout << "For the equation ax^2 + bx + c = 0 write, what the coefficients a, b, c:" << endl;
 	cin >> a >> b >> c;
 }
 
-void solve_linear(double b, double c) {
+void solve_linear(double b, double c, string& out) {
 	if (b == 0) {
 		if (c == 0) {
-			cout << "Infinitely many solutions" << endl;
+			out = "Infinitely many solutions\n";
 		}
 		else {
-			cout << "No solutions" << endl;
+			out = "No solutions\n";
 		}
 	}
 	else {
-		cout << "Only one solution, x = " << -c / b << endl;
+		out = "Only one solution, x = " + to_string(-c / b) + "\n";
 	}
 }
 
@@ -26,36 +33,36 @@ double discriminant(double a, double b, double c) {
 	return b * b - 4 * a * c;
 }
 
-void real_solve(double a, double b, double D) {
+void real_solve(double a, double b, double D, string& out) {
 	double x1 = (-b + sqrt(D)) / (2 * a);
 	double x2 = (-b - sqrt(D)) / (2 * a);
-	cout << "Two real solutions: \n x1 = " << x1 << "\n x2 = " << x2 << endl;
+	out = "Two real solutions: \n x1 = " + to_string(x1) + "\n x2 = " + to_string(x2);
 }
 
-void single_solve(double a, double b) {
-	cout << "One real solution, x = " << (-b) / (2 * a) << endl;
+void single_solve(double a, double b, string& out) {
+	out = "One real solution, x = " + to_string((-b) / (2 * a));
 }
 
-void complex_solve(double a, double b, double D) {
+void complex_solve(double a, double b, double D, string& out) {
 	double real_s = (-b) / (2 * a);
 	double imaginary_s = sqrt(-D) / (2 * a);
-	cout << "Complex solves: \n x1 = " << real_s << " + " << imaginary_s << "i" << "\n x2 = " << real_s << " - " << imaginary_s << "i" << endl;
+	out = "Complex solves: \n x1 = " + to_string(real_s) + " + " + to_string(imaginary_s) + "i" + "\n x2 = " + to_string(real_s) + " - " + to_string(imaginary_s) + "i";
 }
 
-void solve(double a, double b, double c) {
+void solve(double a, double b, double c, string& out) {
 	if (a == 0) {
-		solve_linear(b, c);
+		solve_linear(b, c, out);
 	}
 	else {
 		double D = discriminant(a, b, c);
 		if (D > 0) {
-			real_solve(a, b, D);
+			real_solve(a, b, D, out);
 		}
 		else if (D == 0) {
-			single_solve(a, b);
+			single_solve(a, b, out);
 		}
 		else {
-			complex_solve(a, b, D);
+			complex_solve(a, b, D, out);
 		}
 	}
 
@@ -63,7 +70,10 @@ void solve(double a, double b, double c) {
 
 int main() {
 	double a, b, c;
+	string output_file = "solves.txt";
+	string res;
 	coefficients(a, b, c);
-	solve(a, b, c);
+	solve(a, b, c, res);
+	write_to_file(output_file, res);
 	return 0;
 }
